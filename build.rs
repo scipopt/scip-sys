9f487b3499be4fd02e2cd84031e04d6822266fcd
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-04-06 10:08:08 +0200

    Use pic version of soplex

--- build.rs
+++ build.rs
@@ -141,7 +141,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("cargo:rustc-link-lib=static=gmp");
         println!("cargo:rustc-link-lib=static=gmpxx");
         println!("cargo:rustc-link-lib=static=ipopt");
-        println!("cargo:rustc-link-lib=static=soplex");
+        println!("cargo:rustc-link-lib=static=soplex-pic");
         println!("cargo:rustc-link-lib=static=z");
         println!("cargo:rustc-link-lib=static=scip");
         println!("cargo:rustc-link-lib=dylib=c++");
