6d4feb5c9c9967778703bab01058a298b1b7ce80
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-10-29 20:39:22 +0100

    Fix ipopt linking on windows

--- build.rs
+++ build.rs
@@ -137,9 +137,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     #[cfg(windows)]{
         println!("cargo:rustc-link-lib=static=libscip");
-        println!("cargo:rustc-link-lib=static=libipopt");
         println!("cargo:rustc-link-lib=static=libsoplex");
-        println!("cargo:rustc-link-lib=static=libz");
+        println!("cargo:rustc-link-lib=static=ipopt");
     }
     #[cfg(not(windows))]
     {
