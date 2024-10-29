7093380b0b56679f7cb1c8639c2e76c92b6d8441
Mohammed Ghannam <mohammad.m.ghannam@gmail.com>
2024-10-29 18:06:15 +0100

    Link c++ libs

--- build.rs
+++ build.rs
@@ -142,16 +142,17 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("cargo:rustc-link-lib=static=gmp");
         println!("cargo:rustc-link-lib=static=gmpxx");
         println!("cargo:rustc-link-lib=static=ipopt");
-        println!("cargo:rustc-link-lib=static=soplex-pic");
+        println!("cargo:rustc-link-lib=static=soplex");
         println!("cargo:rustc-link-lib=static=z");
         println!("cargo:rustc-link-lib=static=scip");
         println!("cargo:rustc-link-lib=lapack");
         println!("cargo:rustc-link-lib=blas");
     }
-    //
-    // #[cfg(darwin)]
-    // println!("cargo:rustc-link-lib=dylib=c++");
-    // #[cfg(linux)]
+
+    #[cfg(darwin)]
+    println!("cargo:rustc-link-lib=dylib=c++");
+
+    #[cfg(linux)]
     println!("cargo:rustc-link-lib=stdc++");
 
 
